d9b5780717e24de6806e30393ccc11a411be9ede
mamochi <mamochizoiji@gmail.com>
2019-06-20 14:19:18 +0700

    runner
    runner

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1561015108,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite16-sheet0.png",
+		"images/sprite16-sheet1.png",
+		"images/sprite16-sheet2.png",
+		"images/sprite16-sheet3.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
